5f32d41ffc181f91b97c3664e3ce5b822246754c
iamaashwin99 <iamaashwin99@gmail.com>
2020-07-26 21:52:12 +0530

    pdf_complete

--- md52metadata.py
+++ md52metadata.py
@@ -1,8 +1,17 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+##Imports
 import hashlib
 import os
+import requests
+from bs4 import BeautifulSoup 
+import pandas as pd
+import pprint
+from PyPDF2 import PdfFileReader, PdfFileMerger
 
 
-
+# %%
 def getmd5sum(filename):
 
     md5_hash = hashlib.md5()
@@ -12,11 +21,135 @@ def getmd5sum(filename):
     digest = md5_hash.hexdigest()
     return digest
 
+
+# %%
 def getdatafrommd5(md5):
+    url ='http://gen.lib.rus.ec/book/index.php?md5=' +md5
+    page = requests.get(url) 
+    contents = page.content 
+    soup = BeautifulSoup(contents, 'html.parser')
+    a=soup.body.table.tbody
+    #a = soup.find(id='bibtext')
+    print(a)
+    return #[title, author, publisher, series, year]
 
 
-    return [title, author, publisher, series, year]
+# %%
+def getdatafrommd5v2(md5):
+    url ='http://gen.lib.rus.ec/book/index.php?md5=' +md5
+    print(url)
+    r = requests.get(url)
+    if(r.text == "No record with such MD5 hash has been found</body></html>"):
+        return  [-1, -1, -1, -1, -1]
+    df_list = pd.read_html(r.text) # this parses all the tables in webpages to a list
+    table = df_list[0]
+    title = table.loc[table[1] == 'Title:',2].tolist()[0]
+    author = table.loc[table[1] == 'Author(s):',2].tolist()[0]
+    publisher = table.loc[table[1] == 'Publisher:',2].tolist()[0]
+    series = table.loc[table[1] == 'Series:',2].tolist()[0]
+    year = table.loc[table[1] == 'Year:',2].tolist()[0]
+    return  [title, author, publisher, series, year]
 
 
-ans = getmd5sum('a.djvu')
-print(ans)
+# %%
+def setpdfmetadata(file,title, author, publisher, series, year):
+    newfname='new_'+file
+    file_in = open(file, 'rb')
+    pdf_reader = PdfFileReader(file_in)
+    metadata = pdf_reader.getDocumentInfo()
+    pprint.pprint(metadata)
+
+    pdf_merger = PdfFileMerger()
+    pdf_merger.append(file_in)
+    pdf_merger.addMetadata({
+        u'/Author': author,
+        u'/Title': title,
+        u'/Publisher':str(publisher),
+        u'/Series':str(series),
+        u'/Year':str(year)
+    })
+    file_out = open(newfname, 'wb')
+    pdf_merger.write(file_out)
+    file_in.close()
+    file_out.close()
+  #  with open(file) as f:
+   #     f.fileinfo = {'title': title,'author':author,'publisher':publisher,'series':series,'year':year}
+
+
+
+# %%
+#For file
+#get md5
+#get metadata 
+#set metadata
+
+
+# %%
+
+
+
+# %%
+def doIt(fname):
+    md5 = getmd5sum(fname)
+    [title, author, publisher, series, year] = getdatafrommd5v2(md5)
+    if(title != -1):
+        setpdfmetadata(fname,title, author, publisher, series, year)
+    else:
+        print("nothing found for "+fname)
+
+
+# %%
+for file in os.listdir():
+    if not file.endswith(".pdf"):
+        continue
+    doIt(file)
+
+# %% [markdown]
+# # WorkBench
+
+# # %%
+# ans = getmd5sum('a.pdf')
+# print(ans)
+
+
+# # %%
+# [title, author, publisher, series, year] = getdatafrommd5v2('bfd37427ea4784d215ddb4c4da49ca05')
+
+
+# # %%
+# table=test[0]
+
+
+# # %%
+# ans = table.loc[table[1] == 'Title:',2]
+
+
+# # %%
+# ans.to_string()
+
+
+# # %%
+# ans.tolist()[0]
+
+
+# # %%
+# title
+
+
+# # %%
+# setpdfmetadata('a.pdf',title, author, publisher, series, year)
+
+
+# # %%
+
+# str(series)
+
+
+# # %%
+# doIt('b.pdf')
+
+
+# # %%
+
+
+
